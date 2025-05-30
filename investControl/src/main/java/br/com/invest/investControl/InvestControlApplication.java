00f8349147b4c68a0272695878564be64ecaf253
Jr Marques <49255909+lrmarquesjunior@users.noreply.github.com>
2025-05-29 21:37:38 -0300

    Add files via upload

---
+++ investControl/src/main/java/br/com/invest/investControl/InvestControlApplication.java
@@ -0,0 +1,13 @@
+package br.com.invest.investControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestControlApplication.class, args);
+	}
+
+}
---
+++ investControl/src/main/java/br/com/invest/investControl/controller/CreateUserDto.java
@@ -0,0 +1,11 @@
+package br.com.invest.investControl.controller;
+
+public record CreateUserDto(String username, String email, String password) {
+
+
+
+
+
+
+
+}
